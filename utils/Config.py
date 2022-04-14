db59772b2752092fcee6ce326ac33ddd79b5279d
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2022-04-15 07:49:03 +0800

    Update Config.py

--- utils/Config.py
+++ utils/Config.py
@@ -44,14 +44,14 @@ class Config(metaclass=Singleton):
 
         ## no need to modify these
         if "BOT_URL" not in self.config or not self.config["BOT_URL"]:
-            self.config["BOT_URL"] = "https://t.me/vcplayerbot"
+            self.config["BOT_URL"] = "https://t.me/musika_amigos_musicbot"
         if "PARENT_URL" not in self.config or not self.config["PARENT_URL"]:
             self.config["PARENT_URL"] = "https://t.me/sktechhub"
         if "SUPPORT_GROUP" not in self.config or not self.config["SUPPORT_GROUP"]:
             self.config["SUPPORT_GROUP"] = "https://t.me/voicechatsupport"
         if "GITHUB_REPO" not in self.config or not self.config["GITHUB_REPO"]:
-            self.config["GITHUB_REPO"] = "https://github.com/kshubham506/vcplayerbot"
-        self.config["SUDO_USER"] = [563365858]
+            self.config["GITHUB_REPO"] = "https://github.com/iamerosniko/vcplayerbot"
+        self.config["SUDO_USER"] = [5169659424]
         self.config["PROMOTIONAL_DATA"] = {}
 
         """
